17e9fe3bb9b33810bfbd351d8e3b5e4489c4083a
Egio, Alfonso <alfonso.egio@i2cat.net>
2024-09-28 18:25:41 +0200

    Some renaming / refactor

--- main.el
+++ main.el
@@ -8,27 +8,26 @@
      (fill-region (point-min) (point-max))
      (buffer-substring (point-min) (point-max))))
 
-(defun improve-region-prompt (start end prompt)
-  (progn
-    (insert (format "\n\n🥪 JARVIS INPUT BEGIN 👩🏼‍🔧\n\n%s🥪 JARVIS INPUT END 🕵🏼\n\n"
-                    (fill-string
-                     (shell-command-to-string
-                      (format "curl -s -H 'Content-Type: application/json' -d \
+(defun prompt-gemini-region (start end prompt)
+  (insert (format "\n\n🥪 JARVIS INPUT BEGIN 👩🏼‍🔧\n\n%s🥪 JARVIS INPUT END 🕵🏼\n\n"
+                  (fill-string
+                   (shell-command-to-string
+                    (format "curl -s -H 'Content-Type: application/json' -d \
                                '{\"contents\":[{\"parts\":[{\"text\":\"%s:\n\n%s\"}]}]}' \
                                -X POST 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=%s' \
                                | jq -r -M .candidates[0].content.parts[0].text"
-                              prompt
-                              (trim-lines (buffer-substring (mark) (point)))
-                              gemini-api-key)))))))
+                            prompt
+                            (trim-lines (buffer-substring (mark) (point)))
+                            gemini-api-key))))))
 
 (defun translate-to-english (start end)
   (interactive "r")
-  (improve-region-prompt start end "Can you please translate this to english?"))
+  (prompt-gemini-region start end "Can you please translate this to english?"))
 
 (defun improve-region (start end)
   (interactive "r")
-  (improve-region-prompt start end "Hello, please, help me improve and correct this text"))
+  (prompt-gemini-region start end "Hello, please, help me improve and correct this text"))
 
-(defun refactor-this-code (start end)
+(defun analyze-this-code (start end)
   (interactive "r")
-  (improve-region-prompt start end "Hello, please, provide me a code refactor"))
+  (prompt-gemini-region start end "Hello, please, analyze this source code"))
